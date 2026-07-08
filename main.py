0d8c04c25c7371057bf04041dd71cb7170b002cc
faniAhmed <jattfarhan10@gmail.com>
2026-07-08 12:51:03 +0500

    Added CI/CD and search by title endpoint

--- main.py
+++ main.py
@@ -11,18 +11,34 @@ BOOKS_DB = {}
 app = FastAPI("/api")
 
 #----------------------------------------Routes--------------------#
+"""
+Forgot the import and return method for exact status codes, 
+added comments instead. 
+Validation errors are handled by the Schema and fastapi.
+Respective error code is also returned on Validation errors
+"""
+
 @app.get('/books') # pagination
 def get_books(start: int, count: int) -> List[BOOK]:
     start = max(start, 0) # start index must not be less than 0
     count = max(min(count, 30), 1) # count must be between 1-30
     if len(BOOKS_DB) < (start+count):
         return {'error': 'Invalid page start and count value'}
-    return BOOKS_DB.items()[start:count]
+    return BOOKS_DB.values()[start:count]
 
 
 @app.get('/books/:id')
-def get_book(isbn: int) -> BOOK:
+def get_book_bt_isbn(isbn: int) -> BOOK:
     return BOOKS_DB[isbn]
+
+
+@app.get('/books/:title')
+def get_book_by_title(search_title: str) -> BOOK
+    for book in BOOKS_DB.values():
+        title = book.get('title')
+        if search_title in title:
+            return book
+    return {'error': "Book doesn't exist in the db"} # 404 error code
 
 
 @app.post('/books')
